77cf390dee6d399e87a0611eed3ef95bdf9ca07e
77web <info@77-web.com>
2014-12-03 22:40:44 +0900

    default date.timezone setting

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set('Asia/Tokyo');
+
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 /** @var $loader \Composer\Autoload\ClassLoader */
 $loader->addPsr4('Japanese\Holiday\\', __DIR__);
